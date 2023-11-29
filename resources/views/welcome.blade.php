66f0f1fcef7e705de8c21adc09933ce31e422c37
dimasgilang <105497804+ddim03@users.noreply.github.com>
2023-11-29 21:14:33 +0700

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel + Flowbite</title>
+    @vite(['resources/css/app.css','resources/js/app.js'])
+</head>
+
+<body>
+    <div class="w-full min-h-screen flex justify-center items-center bg-slate-800">
+        <div class="text-center">
+            <h1 class="text-4xl font-sans font-bold text-slate-200 mb-4">Laravel + Flowbite</h1>
+            <span class="text-md font-sans font-light text-slate-200">
+                Use Flowbite Components in <code class="bg-slate-500 p-1 rounded">resources/view/*</code>
+            </span>
+        </div>
+    </div>
+</body>
+
+</html>
